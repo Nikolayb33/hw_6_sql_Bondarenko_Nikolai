60f53e70e89923c29f5e13f66842b2b139500291
Nikolayb33 <nikolayb33@gmail.com>
2024-06-08 13:59:32 +0300

    change hw_6

--- hw_6.sql
+++ hw_6.sql
@@ -1,21 +1,25 @@
 -- задание 1 дз 
-drop procedure times1;
+drop function times1;
 DELIMITER $$
-create procedure times1 (seconds int)	
+create function times1 (seconds int)
+returns char(255)	
+deterministic
 begin
 declare days char(3);
 declare hours, minutes, sec char(2);
 declare result char(255);
+
+
 set days = cast(floor(seconds/60/60/24) as char(3));
 set hours = cast(floor(mod(seconds/60/60/24,1)*24) as char(2));
 set minutes = cast(floor(mod(mod(seconds/60/60/24,1)*24,1)*60) as char(2));
 set sec = cast(round(mod(mod(mod(seconds/60/60/24,1)*24,1)*60,1)*60) as char(2));
 set result = concat(days, "days", hours, "hours", minutes, "minutes", sec, "seconds");
-select result;
+return result;
 end $$
 DELIMITER ;
 
-call times1(125667); -- проверка 1 задания 
+select times1(125667);-- проверка 1 задания 
 
 -- задание 2 дз 
 drop procedure input_chetn;
